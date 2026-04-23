1753b55a15fb2925a64d50be2ab30d62da0f6d6f
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2026-04-23 11:08:49 -0700

    Use PcbBoard typing for tolerance map keys

--- lib/jlcpcb-manufacturing-specs.ts
+++ lib/jlcpcb-manufacturing-specs.ts
@@ -4,9 +4,10 @@ export type JlcToleranceMap = Partial<Record<keyof PcbBoard, number>>
 
 export const jlcMinTolerances: JlcToleranceMap = {
   min_trace_width: 0.1,
-  min_via_to_via_clearance: 0.1,
-  min_trace_to_pad_clearance: 0.1,
-  min_pad_to_pad_clearance: 0.1,
+  min_via_hole_edge_to_via_hole_edge_clearance: 0.1,
+  min_plated_hole_drill_edge_to_drill_edge_clearance: 0.15,
+  min_trace_to_pad_edge_clearance: 0.1,
+  min_pad_edge_to_pad_edge_clearance: 0.1,
   min_board_edge_clearance: 0.2,
   min_via_hole_diameter: 0.2,
   min_via_pad_diameter: 0.3,
@@ -15,9 +16,10 @@ export const jlcMinTolerances: JlcToleranceMap = {
 // TODO: Update these values
 export const jlcPreferredTolerances: JlcToleranceMap = {
   min_trace_width: 0.2,
-  min_via_to_via_clearance: 0.2,
-  min_trace_to_pad_clearance: 0.2,
-  min_pad_to_pad_clearance: 0.2,
+  min_via_hole_edge_to_via_hole_edge_clearance: 0.2,
+  min_plated_hole_drill_edge_to_drill_edge_clearance: 0.2,
+  min_trace_to_pad_edge_clearance: 0.2,
+  min_pad_edge_to_pad_edge_clearance: 0.2,
   min_board_edge_clearance: 0.2,
   min_via_hole_diameter: 0.4,
   min_via_pad_diameter: 0.6,
